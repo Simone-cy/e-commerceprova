b9a6a5010f25e4585ab755766340da61c60d89da
Simone-cy <155980155+Simone-cy@users.noreply.github.com>
2025-05-28 14:14:41 +0200

    Update DemoAdmin.jsx

--- src/components/admin/DemoAdmin.jsx
+++ src/components/admin/DemoAdmin.jsx
@@ -1,27 +1,30 @@
 /**
+ * DemoAdmin.jsx
+ * 
  * Questo componente implementa una pagina dimostrativa dell'interfaccia di amministrazione.
  * Mostra uno screenshot dell'interfaccia di amministrazione dei prodotti per 
  * scopi di presentazione e demo senza funzionalità interattive.
  */
 import React from 'react';
 import { Container, Typography, Paper, Box } from '@mui/material';
+import adminScreenshot from '../../Screenshot 2025-05-28 122835.png';
 
 function DemoAdmin() {
-    return (
-        <Container sx={{ py: 4 }}>
-            <Typography variant="h4" gutterBottom>
-                Interfaccia di Amministrazione per la gestione dei prodotti - Demo
-            </Typography>
-            <Paper elevation={3} sx={{ p: 3, textAlign: 'center' }}>
-                <Box
-                    component="img"
-                    src="../../Screenshot 2025-05-28 122835.png" 
-                    alt="Interfaccia di Amministrazione"
-                    sx={{ width: '100%', maxWidth: 800, borderRadius: 2 }}
-                />
-            </Paper>
-        </Container>
-    )
+  return (
+    <Container sx={{ py: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Interfaccia di Amministrazione - Demo
+      </Typography>
+      <Paper elevation={3} sx={{ p: 3, textAlign: 'center' }}>
+        <Box
+          component="img"
+          src={adminScreenshot}
+          alt="Interfaccia di Amministrazione"
+          sx={{ width: '100%', maxWidth: 1200, height: 'auto', borderRadius: 2 }}
+        />
+      </Paper>
+    </Container>
+  );
 }
 
 export default DemoAdmin;
